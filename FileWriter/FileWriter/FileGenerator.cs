e14c4cbf0748de4183897d9ce754e52c03ccf131
pedroreis <pedroreiscoder@gmail.com>
2021-01-22 18:20:54 -0300

    Criacao da classe que gera o arquivo e primeiros comandos do usuario sendo lidos

---
+++ FileWriter/FileWriter/FileGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FileWriter
+{
+    public static class FileGenerator
+    {
+        public static void GenerateFile(string path, int fileSize, int bufferSize)
+        {
+            string sentence = WebCrawler.GetSentence();
+            int bytes = WebCrawler.GetBytes(sentence);
+
+            int bufferSizeBytes = bufferSize * 1048576;
+            long fileSizeBytes = fileSize * 1048576;
+
+            int iterations = bufferSizeBytes / bytes;
+            int flushSizeBytes = iterations * bytes;
+            long currentFileSizeBytes = 0;
+
+            string currentDateTime = DateTime.Now.ToString("yyyy-MM-dd-HHmmss");
+
+            using (StreamWriter writer = new StreamWriter($@"{path}\{currentDateTime}-arquivo-gerado.txt", false, Encoding.UTF8, bufferSizeBytes))
+            {
+                do
+                {
+                    for (int i = 0; i < iterations; i++)
+                        writer.Write(sentence);
+
+                    writer.Flush();
+                    currentFileSizeBytes += flushSizeBytes;
+
+                } while (currentFileSizeBytes < fileSizeBytes);
+            }
+        }
+    }
+}
--- FileWriter/FileWriter/Program.cs
+++ FileWriter/FileWriter/Program.cs
@@ -6,9 +6,36 @@ namespace FileWriter
     {
         static void Main(string[] args)
         {
-            string sentence = WebCrawler.GetSentence();
-            Console.WriteLine(sentence);
-            Console.WriteLine(WebCrawler.GetBytes(sentence));
+            Console.WriteLine("Iniciando execução");
+
+            Console.WriteLine("Informe o path do arquivo:");
+            string path = Console.ReadLine();
+
+            Console.WriteLine("Você deseja alterar o tamanho padrão do arquivo (100MB): s ou n");
+            string changeFileSize = Console.ReadLine();
+
+            int fileSize = 100;
+
+            if (changeFileSize == "s")
+            {
+                Console.WriteLine("Informe o tamanho do arquivo:");
+                fileSize = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Você deseja alterar o tamanho máximo do buffer (1MB): s ou n");
+            string changeBufferSize = Console.ReadLine();
+
+            int bufferSize = 1;
+
+            if (changeBufferSize == "s")
+            {
+                Console.WriteLine("Informe o tamanho máximo do buffer:");
+                bufferSize = int.Parse(Console.ReadLine());
+            }
+
+            FileGenerator.GenerateFile(path, fileSize, bufferSize);
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
             Console.ReadLine();
         }
     }
