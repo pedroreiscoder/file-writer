0d433a606f1055e47b56c8b601f923e253177086
pedroreis <pedroreiscoder@gmail.com>
2021-01-23 14:34:00 -0300

    Adicionada validacao nos inputs do usuario

--- FileWriter/FileWriter/Program.cs
+++ FileWriter/FileWriter/Program.cs
@@ -8,10 +8,17 @@ namespace FileWriter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Iniciando execução");
+            Console.WriteLine("--------------- Gerador de Arquivos ---------------");
 
-            Console.WriteLine("Informe o path do arquivo:");
+            Console.Write("Informe o path de escrita do arquivo: ");
             string path = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(path))
+            {
+                Console.WriteLine("O path para escrita do arquivo é obrigatório!");
+                Console.Write("Informe o path de escrita do arquivo: ");
+                path = Console.ReadLine();
+            }
 
             Console.WriteLine("Você deseja alterar o tamanho padrão do arquivo (100MB): s ou n");
             string changeFileSize = Console.ReadLine();
@@ -20,8 +27,15 @@ namespace FileWriter
 
             if (changeFileSize == "s")
             {
-                Console.WriteLine("Informe o tamanho do arquivo:");
+                Console.Write("Informe o tamanho do arquivo em MB: ");
                 fileSize = int.Parse(Console.ReadLine());
+
+                while (fileSize <= 0)
+                {
+                    Console.WriteLine("O tamanho do arquivo deve ser positivo!");
+                    Console.Write("Informe o tamanho do arquivo em MB: ");
+                    fileSize = int.Parse(Console.ReadLine());
+                }
             }
 
             Console.WriteLine("Você deseja alterar o tamanho máximo do buffer (1MB): s ou n");
@@ -31,8 +45,15 @@ namespace FileWriter
 
             if (changeBufferSize == "s")
             {
-                Console.WriteLine("Informe o tamanho máximo do buffer:");
+                Console.Write("Informe o tamanho máximo do buffer em MB: ");
                 bufferSize = int.Parse(Console.ReadLine());
+
+                while (bufferSize <= 0)
+                {
+                    Console.WriteLine("O tamanho máximo do buffer deve ser positivo!");
+                    Console.Write("Informe o tamanho máximo do buffer em MB: ");
+                    bufferSize = int.Parse(Console.ReadLine());
+                }
             }
 
             using (WebCrawler webCrawler = new WebCrawler())
